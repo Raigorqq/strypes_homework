d530ca1dde56d4872704ddfc94a02006a0d5261b
Vadim Tsubin <108688332+Raigorqq@users.noreply.github.com>
2022-07-25 17:19:07 +0300

    Update 0-5.py

--- 22.07_(7)homework/0-5.py
+++ 22.07_(7)homework/0-5.py
@@ -1,16 +1,16 @@
-1 zadacha
+#1 task
 file = open("dog_breeds.txt", "r")
 
 print(file.read())
 file.close()
 
-# 2-3 zadacha
+#2-3 task
 file = open("dog_breeds.txt", "a")
 
 file.write("\n" + "text")
 file.close()
 
-# 4 zadacha
+#4 task
 file = open("dog_breeds.txt", "a")
 
 args = input("Input some text:" )
@@ -18,7 +18,7 @@ args = input("Input some text:" )
 file.write("\n" + args)
 file.close()
 
-5 zadacha
+#5 task
 file_in = open("dog_breeds.txt", "r")
 file_out = open("dog_.txt", "a")
 file_out.write(file_in.read())
