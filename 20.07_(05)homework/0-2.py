aacfa68452751f6423da6388b77b56cd4d11d584
Vadim Tsubin <108688332+Raigorqq@users.noreply.github.com>
2022-07-25 17:29:00 +0300

    Update 0-2.py

--- 20.07_(05)homework/0-2.py
+++ 20.07_(05)homework/0-2.py
@@ -20,18 +20,18 @@ class Entity:
 #0.Hero
 class Hero(Entity):
 
-    def __init__(self, name, nickname, health, damage):
+    def __init__(self, name, nickname, health):
         self.name = name
         self.nickname = nickname
         self.health = health
-        self.damage = damage
+
 
     def __str__(self):
         return ("Hero: " + self.name + " the " + self.nickname)
 
     
 
-h = Hero("Bron", "Dragonslayer", 100, 24)
+h = Hero("Bron", "Dragonslayer", 100)
 
 print(h.name)
 print(h.nickname)
@@ -49,7 +49,7 @@ print(h.health)
 
 #1.Orc
 class Orc(Entity):
-    def __init__(self, name, health, berserk_factor, damage):
+    def __init__(self, name, health):
         self.name = name
         self.health = health
         self.berserk_factor = berserk_factor
@@ -57,11 +57,8 @@ class Orc(Entity):
 
     def __str__(self):
         return ("Orc:" + self.name)
-    
-    def berserker_mod(self):
-        if self.berserk_factor is True:
-            self.damage *= 2
 
-o = Orc("Muha", 101, 1, 25)
+
+o = Orc("Muha", 101)
 
 print(o)
