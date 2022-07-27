cdcde1e3656c4ef253e0b7f84abd0ced72bd6ddc
Vadim Tsubin <108688332+Raigorqq@users.noreply.github.com>
2022-07-27 18:44:59 +0300

    Add files via upload

--- Project/minigame_main.py
+++ Project/minigame_main.py
@@ -9,13 +9,13 @@ class Fight(Human, Ogre, Troll):
         if int(number) == 1:
             location = "Forest"
             enemy = ogre
-        if int(number) == 2:
+        else:
             location = "Cave"
             enemy = troll
         
         print("An ",enemy.name," lives in this" + location + ", fight!\n")
         while(h.health >= 0) or (enemy.health >= 0):
-                hw = input(h.name +", 1.Attack or 2.Heal?:" )
+                hw = input("\n" + h.name +",\n 1.Attack?\n 2.Heal?" )
 
                 if hw == "1":
                     enemy.take_damage(h.damage)
@@ -25,7 +25,7 @@ class Fight(Human, Ogre, Troll):
                     while h.snumber > 0:
                         h.take_healing(h.healing_points)
                         h.snumber -= 1
-                        print(h.name, " health: ", h.health, " \nSalves remaining: ", h.snumber)
+                        print("\n" + h.name, " health: ", h.health, " \nSalves remaining: ", h.snumber)
                         break
                     if h.health > 450:
                         print("Overdose, lol")
@@ -41,20 +41,20 @@ class Fight(Human, Ogre, Troll):
                     print(h.name," health: ", h.health)
                 else:
                     h.take_damage(enemy.damage)
-                    print(enemy.name," Attacks you!\n",h.name," health:", h.health)
+                    print("\n" + enemy.name," Attacks you!\n",h.name," health:", h.health)
 
                 if h.health <= 0:
-                    print("Game Over")
+                    print("\nGame Over\n")
                     break
                 if enemy.health <= 0:
                     print("You got dragon potion from " + enemy.nickname)
                     break
         
 class BossFight(Dragon):
-    def bfight():
+    def BFight():
      print("Final battle,", h.name, "!\n", dragon)
      while(h.health >= 0) or (dragon.health >= 0):
-        hw = input(h.name +", 1.Attack, 2.Heal or 3.Use your Potion?:" )
+        hw = input("\n"+ h.name +",\n 1.Attack?\n 2.Heal?\n 3.Use your Potion?" )
 
         if hw == "1":
             if h.sword_status != 50:
@@ -78,16 +78,16 @@ class BossFight(Dragon):
 
         if hw == "3":
             h.potion(50)
-            print("Using potion...\nYou put poison on your sword!\n Sword damage:", h.damage + h.sword_status)
+            print("Using potion...\nYou used poison on your sword!\n Sword damage:", h.damage + h.sword_status)
 
         h.take_damage(dragon.damage)
         print(dragon.name," Attacks you!\n",h.name," health:", h.health) 
 
         if h.health <= 0:
-                print("Game Over")
+                print("\nGame Over\n")
                 break
         if dragon.health <= 0:
-                print("YOU DEFEAT ", dragon.name, "!\n THE CHEESE IS YOUR!!")
+                print("\nYOU DEFEAT ", dragon.name, "!\n THE CHEESE IS YOUR!!")
                 break
 # ------------------------------------------------------------------------
 
@@ -119,15 +119,15 @@ dragon.spell("Thick skin", "Skin reduce damage by 20", 20)
 # ------------------------------------------------------------------------
 
 #GAME:
-print("Welcome,", h, "\nYour mission is to steal CHEESE from Dragon!\n\nWhose your way(enter number):\n 1.Forest\n 2.Cave")
+print("Welcome,", h, "\nYour mission is to steal CHEESE from Dragon!\n\nChoose your way:\n 1.Forest\n 2.Cave")
 number = input()
 if int(number) < 1 or int(number) > 2 :
-    print("Wrong Number...\n Whose your way(enter number):\n 1.Forest\n 2.Cave")
+    print("Wrong Number...\n Choose your way(enter number):\n 1.Forest\n 2.Cave")
     number = input()
 e = Fight
 e.EnemyFight(number)
 
 boss = BossFight
 if h.is_alive() == True:
-    boss.bfight()
+    boss.BFight()
 # ------------------------------------------------------------------------
