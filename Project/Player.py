fb1182d935d8725faf6ec341adda0632971083ec
Vadim Tsubin <108688332+Raigorqq@users.noreply.github.com>
2022-07-27 18:49:14 +0300

    Update Player.py

--- Project/Player.py
+++ Project/Player.py
@@ -6,7 +6,3 @@ class Human(Info, Check, Spell, Salve, Sword):
 
     def __str__(self):
         return (self.name + " the " + self.nickname)
-
-h = Human("Pudge", "Butcher", 250, 25)
-h.health_salve("Healing Salve", 125, 5)
-h.potion(0)
